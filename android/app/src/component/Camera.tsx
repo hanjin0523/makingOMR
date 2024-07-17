7de4c99f2001e653b090d9755859a7f6a982171b
hanjin0523 <anjdi1004@gmail.com>
2024-07-17 17:56:13 +0900

    2커밋

--- android/app/src/component/Camera.tsx
+++ android/app/src/component/Camera.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 function Camera(): React.JSX.Element {
-  return (
-    <View style={styles.container}>
-      <Text>Camera</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Camera</Text>
+            <Button title="카메라동작" onPress={() => { }} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
 
 export default Camera;
